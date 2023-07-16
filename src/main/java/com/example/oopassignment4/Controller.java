8fed126aa4288e2362ce5382ec31a22868551e74
Christian <200390696@student.georgianc.on.ca>
2023-07-16 10:20:57 -0400

    Implement Initializable(), add UML

--- src/main/java/com/example/oopassignment4/Controller.java
+++ src/main/java/com/example/oopassignment4/Controller.java
@@ -1,14 +1,23 @@
 package com.example.oopassignment4;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
